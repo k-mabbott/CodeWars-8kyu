76dc963fbfe771bcd8c176880a6ff81ae2f2f2c9
k-mabbott <128265569+k-mabbott@users.noreply.github.com>
2023-04-08 21:07:20 -0700

    Add files via upload

---
+++ reversedStrings.js
@@ -0,0 +1,11 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+    let result = ""
+    for (var i = str.length-1; i >= 0; i--){
+        result += str[i];
+    } return result;
+}
